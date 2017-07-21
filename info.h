05158555b2dd58477847395ff0f6cda9f480566d
Oleg Kutkov <elenbert@gmail.com>
2017-07-22 01:04:10 +0300

    general module info moved to separate file

---
+++ info.h
@@ -0,0 +1,30 @@
+/*
+   info.c
+	- general module info
+
+   Copyright 2017  Oleg Kutkov <elenbert@gmail.com>
+                   Crimean astrophysical observatory
+
+   This program is free software: you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation, either version 3 of the License, or
+   (at your option) any later version.
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+   You should have received a copy of the GNU General Public License
+   along with this program.  If not, see <http://www.gnu.org/licenses/>.  
+ */
+
+#ifndef LIR_INFO_H
+#define LIR_INFO_H
+
+#define DRIVER_NAME "lir941r"
+#define DRIVER_VERSION "0.1"
+#define DRIVER_DESCRIPTION "LIR941R encoders interface driver"
+#define DRIVER_COPYRIGHT "Copyright (c) 2017 Oleg Kutkov. Crimean astrophysical observatory"
+
+
+#endif
+
